028aa1c00a915d66f10ea48277f24c40bce74ed2
matiespinel <matiasespinel.me@gmail.com>
2023-08-10 14:23:18 -0300

    Update SpawnPlayers.cs

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
