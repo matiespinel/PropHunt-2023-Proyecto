6974bbc82e71dafa268900a9ac93dcbd89255b27
GoGonus <gonzalo.gnus@gmail.com>
2023-08-24 13:49:08 -0300

    intente un arreglo desactivando...

--- Assets/Scripts/AudioListenerMine.cs
+++ Assets/Scripts/AudioListenerMine.cs
@@ -8,20 +8,15 @@ public class AudioListenerMine : MonoBehaviour
     private AudioListener audioListener;
     PhotonView view;
     [SerializeField] CinemachineFreeLook cam3d;
-    [SerializeField] CinemachineBrain solocam;
-    [SerializeField] GameObject w1;
-    [SerializeField] GameObject w2;
     void Start()
     {
-        //cam3d.LookAt = w1.transform;
-        //cam3d.Follow = w2.transform;
-        CinemachineBrain.SoloCamera = cam3d;
         view = GetComponent<PhotonView>();
         audioListener = GetComponent<AudioListener>();
 
         if (!view.IsMine)
         {
             audioListener.gameObject.SetActive(false);
+            cam3d.gameObject.SetActive(false);
 
         }
     }
