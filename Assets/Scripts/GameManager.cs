2d37d81da90596b9e0bcdfac0dfa3431c3d34f15
matiespinel <matiasespinel.me@gmail.com>
2023-08-25 08:58:51 -0300

    Destruir paredes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,18 +5,35 @@ using Photon.Pun;
 using Photon.Realtime;
 
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
+
+
+    [SerializeField] GameObject Pared1;
+    [SerializeField] GameObject Pared2;
+    [SerializeField] GameObject Pared3;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hacer coroutine para destruir las paredes
+        StartCoroutine("destruirParedes");
+
     }
+   
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    
+     IEnumerator destruirParedes()
+    {
+        yield return new WaitForSeconds(15);
+        Pared1.gameObject.SetActive(false);
+        Pared2.gameObject.SetActive(false);
+        Pared3.gameObject.SetActive(false);
     }
 }
