b06a7eddd8ac6135a58af5909c70ca9c1d44cf22
matiespinel <matiasespinel.me@gmail.com>
2023-09-08 09:29:26 -0300

    Update RoleAssigner.cs

--- Assets/Scripts/RoleAssigner.cs
+++ Assets/Scripts/RoleAssigner.cs
@@ -47,7 +47,7 @@ public class RoleAssigner : MonoBehaviourPunCallbacks
         {
 
 
-            Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), 2,-40);
+            Vector3 randomPosition = new Vector3(-4, 2,-4);
             PhotonNetwork.Instantiate(playerPrefab2.name, randomPosition, Quaternion.identity);
             Debug.Log("Prop");
             gm.propCount++;
