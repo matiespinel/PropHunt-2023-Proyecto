df0afd7f4ccefeabe02ae71b445f2a16432de17c
GoGonus <gonzalo.gnus@gmail.com>
2023-09-28 13:58:20 -0300

    elimino sript inutil y trato mejorar apuntado

--- Assets/Scripts/GunShotScript.cs
+++ Assets/Scripts/GunShotScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GunShotScript : Weapon
 {
     private Entity youEntity;
+
     void Start()
     {
         #region bullet Attributes
---
+++ Assets/Scripts/LookingTargetRecenter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LookingTargetRecenter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lookingTarget;
+
+    [SerializeField]
+    private LayerMask ignoreLayer;
+
+    private Camera cam;
+    private PhotonView view;
+    private RaycastHit hit;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        view = GetComponent<PhotonView>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.IsMine) 
+        {
+ 
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast(ray, out hit, 100, ignoreLayer))
+            {
+                if(lookingTarget.transform.position != hit.point) 
+                {
+                    Debug.Log("vvv");
+                    lookingTarget.transform.localPosition = new Vector3(lookingTarget.transform.localPosition.x, lookingTarget.transform.localPosition.y, hit.point.z);
+                }
+               
+            }
+        }
+    }
+}
