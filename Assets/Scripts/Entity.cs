eca844b1721c87abed025144505a576941857885
GoGonus <gonzalo.gnus@gmail.com>
2023-09-29 10:27:38 -0300

    El respawn debera arreglarse
    Tener en cuenta que desde un cliente no se puede hacer photondestroy a un GO de otro cliente, debe ser el propio owner quien lo haga.

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -26,7 +26,6 @@ public abstract class Entity : MonoBehaviour
         Debug.Log(HP);
         if(HP <= 0)
         {
-            PhotonNetwork.Destroy(this.gameObject);
             OnEntityDeath?.Invoke();//Este evento permitira conectar scripts que se "activaran" al momento de la muerte. Usar esto para respawn
         }
     }
