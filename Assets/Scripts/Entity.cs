8c2516f7404f03dc89c6b8eb22aed00af6d69c04
GoGonus <gonzalo.gnus@gmail.com>
2023-10-02 14:52:29 -0300

    sanwich

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using System;
 
-public abstract class Entity : MonoBehaviour
+public abstract class Entity : MonoBehaviourPunCallbacks
 {
     #region attributes
     public int HP{get; set;}
