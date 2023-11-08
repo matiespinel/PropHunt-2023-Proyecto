9e686930eaf9e9e883fd2eea215c096886d5e28e
matiespinel <matiasespinel.me@gmail.com>
2023-11-08 09:53:33 -0300

    luz2

---
+++ Assets/Scripts/LuzParpadea.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuzParpadea : MonoBehaviour
+{
+
+    [SerializeField] Light luz;
+    [SerializeField] Light luz2;
+    [SerializeField] Light luz3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // hacer secuencia de luz que parpadea
+        luz.intensity = Random.Range(0.5f, 5f);
+        luz2.intensity = Random.Range(0.5f, 5f);
+        luz3.intensity = Random.Range(0.5f, 5f);
+
+    }
+}
