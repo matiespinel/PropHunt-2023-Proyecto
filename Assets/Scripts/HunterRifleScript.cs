1d4108588b49ccfb55eb79112eda6ea0f98482b8
GoGonus <110360064+GoGonus@users.noreply.github.com>
2023-11-12 18:46:52 -0300

    camvbivitos al mapa

--- Assets/Scripts/HunterRifleScript.cs
+++ Assets/Scripts/HunterRifleScript.cs
@@ -22,7 +22,7 @@ public class HunterRifleScript : Weapon
         mag = 20;
         initialMag = mag;
         ammo = 100;
-        reloadTime = new WaitForSeconds(3.5f);
+        reloadTime = new WaitForSeconds(2.8f);
        
         #endregion
         youEntity = GetComponent<Entity>();
